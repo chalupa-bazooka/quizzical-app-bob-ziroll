2fa5afb52c69947ce6cac0f3651cf8bc9a59a3f6
chalupa-bazooka <mteplyshev@gmail.com>
2024-01-12 22:09:03 +0400

    Change it to our values

--- src/components/RadioButtonsTest.jsx
+++ src/components/RadioButtonsTest.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 export default function RadioButtonTest() {
-    const [profile, setProfile] = useState({
-        sleptLastNightAt: ""
+    const [answers, setAnswers] = useState({
+        goodbyeInSpanish: ""
     })
 
     function handleChange(event) {
         console.log(event.target.name, event.target.value)
-        setProfile((prevProfile) => {
-            return { ...prevProfile, [event.target.name]: event.target.value }
+        setAnswers((prevAnswers) => {
+            return { ...prevAnswers, [event.target.name]: event.target.value }
         })
     }
 
@@ -16,42 +16,54 @@ export default function RadioButtonTest() {
         <div className="QuizPage__General">
             <h1>sup</h1>
             <fieldset>
-                <p>Where did you sleep last night?</p>
+                <p>How would one say goodbye in Spanish?</p>
 
                 <label htmlFor="radio1">
                     <input
                         type="radio"
                         id="radio1"
-                        name="sleptLastNightAt"
-                        value="at home"
-                        checked={profile.sleptLastNightAt === "at home"}
+                        name="goodbyeInSpanish"
+                        value="adiós"
+                        checked={answers.goodbyeInSpanish === "adiós"}
                         onChange={handleChange}
                     />
-                    At home
+                    Adiós
                 </label>
 
                 <label htmlFor="radio2">
                     <input
                         type="radio"
                         id="radio2"
-                        name="sleptLastNightAt"
-                        value="in the pines"
-                        checked={profile.sleptLastNightAt === "in the pines"}
+                        name="goodbyeInSpanish"
+                        value="hola"
+                        checked={answers.goodbyeInSpanish === "hola"}
                         onChange={handleChange}
                     />
-                    In the pines
+                    Hola
                 </label>
 
                 <label htmlFor="radio3">
                     <input
                         type="radio"
                         id="radio3"
-                        name="sleptLastNightAt"
-                        value="won't tell"
-                        checked={profile.sleptLastNightAt === "won't tell"}
+                        name="goodbyeInSpanish"
+                        value="au revoir"
+                        checked={answers.goodbyeInSpanish === "au revoir"}
                         onChange={handleChange}
                     />
-                    IDK, piss off!
+                    Au Revoir
+                </label>
+
+                <label htmlFor="radio4">
+                    <input
+                        type="radio"
+                        id="radio4"
+                        name="goodbyeInSpanish"
+                        value="salir"
+                        checked={answers.goodbyeInSpanish === "salir"}
+                        onChange={handleChange}
+                    />
+                    Salir
                 </label>
             </fieldset>
         </div>
