dff69108602e9826090179b8c9fd2ed713acfed4
chalupa-bazooka <mteplyshev@gmail.com>
2024-01-14 14:46:38 +0400

    Add the mapping logic to Question

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,38 @@
-export default function Question({question}) {
+import {nanoid} from "nanoid";
+
+export default function Question({ question, answers, handleChange }) {
     return (
         <div className="Question__General">
             <p className="Question__Title">{question.title}</p>
-            <div className="Question__AnswersRow">
-                <div className="Question__SingleAnswer">{question.answer1.answer}</div>
-                <div className="Question__SingleAnswer">{question.answer2.answer}</div>
-                <div className="Question__SingleAnswer">{question.answer3.answer}</div>
-                <div className="Question__SingleAnswer">{question.answer4.answer}</div>
-            </div>
+
+            {/* row of answers */}
+            <fieldset className="Question__AnswersRow">
+                {
+                    // mapping answer options (objects inside answerOptions array from the "questions" state)
+                    question.answerOptions.map((answerOption) => {
+                        return (
+                            <label
+                                className=
+                                {answers[question.relatedAnswersProperty] === answerOption.answer
+                                    ? "Question__SingleAnswerCheckedTest"
+                                    : "Question__SingleAnswerTest"}
+                                key={nanoid()}
+                            >
+                                <input
+                                    className="Question__SingleAnswerTestInput"
+                                    type="radio"
+                                    name={question.relatedAnswersProperty}
+                                    value={answerOption.answer}
+                                    checked={answers[question.relatedAnswersProperty] === answerOption.answer}
+                                    onChange={handleChange}
+                                />
+                                {answerOption.answer}
+                            </label>
+                        )
+                    })
+                }
+            </fieldset>
+
         </div>
     )
 }
