1f160a2fc025978251f0410027ac966241d5fb08
chalupa-bazooka <mteplyshev@gmail.com>
2024-01-14 14:53:16 +0400

    Add the code from RadioButtosnTest

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,15 +1,51 @@
 import Question from "./Question.jsx";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 
-export default function QuizPage({questions}) {
+{/* 
+to do:
+- add submit button
+- on submit we gotta compare our states:
+answers (chosen answers) with questions (if the chosen answers are correct)
+- correct answers gotta be highlighted with green (regardless of if previously selected)
+- wrong answers gotta be highlighted with red (only if selected)
+- if successful, transfer the code to the Question component
+*/}
+
+export default function QuizPage({ questions }) {
+    const [answers, setAnswers] = useState(() => {
+        return questions.map((question) => {
+            return question.relatedAnswerProperty;
+        })
+    })
+
+    function handleChange(event) {
+        setAnswers((prevAnswers) => {
+            return { ...prevAnswers, [event.target.name]: event.target.value }
+        })
+    }
+
     return (
         <div className="QuizPage__General">
-            {questions.map((question) => {
-                return <Question question={question} key={nanoid()} />
-            })}
+
+            {
+                // mapping questions objects (from the "questions" state)
+                questions.map((question) => {
+                    return (
+                        <Question
+                            question={question}
+                            answers={answers}
+                            handleChange={handleChange}
+                            key={nanoid()}
+                        />
+                    )
+                })
+            }
+
             <button className="QuizPage__Button">
                 Check answers
             </button>
+            
             <img
                 src="src/images/quizpage-blob-blue.png"
                 alt="blob blue quiz page"
