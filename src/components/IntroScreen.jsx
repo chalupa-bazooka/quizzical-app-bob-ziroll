6413b85947583d9b28e16d4e186bf871d6609c22
chalupa-bazooka <mteplyshev@gmail.com>
2024-01-19 22:21:47 +0400

    Add condition + quizStart logic

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -1,19 +1,31 @@
-export default function IntroScreen() {
+import QuizPage from "./QuizPage"
+
+export default function IntroScreen({quizStart, setQuizStart}) {
+    function startQuiz(){
+        setQuizStart(true)
+    }
+
     return (
-        <div className="IntroScreen__General">
-            <h1 className="IntroScreen__Title">Quizzical</h1>
-            <p className="IntroScreen__Subtitle">Some description if needed</p>
-            <button className="IntroScreen__Button">Start quiz</button>
-            <img
-                className="IntroScreen__Image--BlobOrange"
-                src="src/images/blob-orange.png"
-                alt="blob orange"
-            />
-            <img
-                className="IntroScreen__Image--BlobBlue"
-                src="src/images/blob-blue.png"
-                alt="blob blue"
-            />
-        </div>
+        <>
+            {
+                !quizStart ?
+                <div className="IntroScreen__General">
+                    <h1 className="IntroScreen__Title">Quizzical</h1>
+                    <p className="IntroScreen__Subtitle">Some description if needed</p>
+                    <button className="IntroScreen__Button" onClick={startQuiz}>Start quiz</button>
+                    <img
+                        className="IntroScreen__Image--BlobOrange"
+                        src="src/images/blob-orange.png"
+                        alt="blob orange"
+                    />
+                    <img
+                        className="IntroScreen__Image--BlobBlue"
+                        src="src/images/blob-blue.png"
+                        alt="blob blue"
+                    />
+                </div>
+                : <QuizPage quizStart={quizStart} setQuizStart={setQuizStart}/>
+            }
+        </>
     )
 }
